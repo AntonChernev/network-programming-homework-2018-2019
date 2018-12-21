8c68769b8fdcc8af0847fecda10d267bf1034e3a
Anton <ss.a.chernev@gmail.com>
2018-12-21 22:42:14 +0200

    Server backbone

---
+++ 81505/src/com/fmi/mpr/hw/http/HttpServer.java
@@ -0,0 +1,53 @@
+package com.fmi.mpr.hw.http;
+
+import java.net.*;
+import java.io.*;
+
+class HttpServer {
+    private ServerSocket server;
+    private final int port;
+    private boolean isRunning;
+
+    HttpServer(int port) throws IOException {
+        server = new ServerSocket(port);
+        this.port = port;
+        isRunning = false;
+    }
+
+    private void listen() throws IOException {
+        Socket connection = null;
+        try {
+            connection = server.accept();
+            System.out.println("New connection: " + connection.getInetAddress());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Closing connection");
+            if(connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    private void run() {
+        while(isRunning) {
+            try {
+                listen();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    int getPort() {
+        return port;
+    }
+
+    void start() {
+        if(!isRunning) {
+            isRunning = true;
+            run();
+        }
+    }
+}
---
+++ 81505/src/com/fmi/mpr/hw/http/Main.java
@@ -0,0 +1,11 @@
+package com.fmi.mpr.hw.http;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = new HttpServer(8888);
+        server.start();
+    }
+}
